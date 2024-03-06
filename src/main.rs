06d2db863dbe75bc50b886244f5f4e2b0f766957
trist4nl3 <s4008982@student.rmit.edu.au>
2024-03-07 09:07:05 +1100

    finished up and fixed up miner

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,8 @@ use tokio::{
 };
 
 
+
+mod p2p;
 /*
 Rules: 1. Previous hash needs to actually match hash of last block in chain
 2. Hash start with Difficulty prefix therefore mined correctly, id needs to be the latest ID ++
@@ -55,6 +57,62 @@ pub struct Block {
     pub timestamp: i64,
     pub data: String,
     pub nonce: u64, // This is the number that we change to get a hash that satisfies our difficulty criteria.
+}
+
+
+impl Block {
+    pub fn new(id: u64, previous_hash: String, data: String) -> Self {
+        let now = Utc::now();
+        let (nonce, hash) = mine_block(id, now.timestamp(), &previous_hash, &data);
+
+        Self {
+            id,
+            hash,
+            previous_hash,
+            timestamp: now.timestamp(),
+            data,
+            nonce,
+        }
+    }
+}
+
+// Create a json representation of block and put it through sha256 hash function
+fn calculate_hash(id: u64, timestamp: i64, previous_hash: &str, data: &str, nonce: u64) -> Vec<u8> {
+    let data = serde_json::json!({
+        "id": id,
+        "timestamp": timestamp,
+        "previous_hash": previous_hash,
+        "data": data,
+        "nonce": nonce // this is the number that we change to get a hash that satisfies our difficulty criteria
+    });
+    let mut hasher = Sha256::new();
+    hasher.update(data.to_string().as_bytes());
+    hasher.finalize().as_slice().to_owned()
+}
+
+fn mine_block(id: u64, timestamp: i64, previous_hash: &str, data: &str) -> (u64, String) {
+    info!("Mining block...");
+    let mut nonce = 0;
+    loop {
+        // Rough progress indicator
+        if nonce % 100000 == 0 {
+            info!("nonce: {}", nonce);
+        }
+        // Trying to find a hash that satisfies the difficulty criteria
+        let hash = calculate_hash(id, timestamp, previous_hash, data, nonce);
+        // To check whether the calculated hash adheres difficulty criteria of starting with two zeros
+        let binary_hash = hash_to_binary_representation(&hash);
+        if binary_hash.starts_with(DIFFICULTY_PREFIX) {
+            info!(
+                "mined! nonce: {}, hash: {}, binary hash: {}",
+                nonce,
+                hex::encode(&hash),
+                binary_hash
+            );
+            return (nonce, hex::encode(hash));
+        }
+        nonce += 1;
+    }
 }
 
 // Implementing App struct
@@ -135,7 +193,7 @@ impl App {
         true
     }
     // Always choose the longest chain
-    fn choose_chain(&mut self, local: Vec, remote: Vec) -> Vec {
+    fn choose_chain(&mut self, local: Vec<Block>, remote: Vec<Block>) -> Vec<Block> {
         let is_local_valid = self.is_chain_valid(&local);
         let is_remote_valid = self.is_chain_valid(&remote);
 
@@ -157,56 +215,102 @@ impl App {
     
 }
 
-impl Block {
-    pub fn new(id: u64, previous_hash: String, data: String) -> Self {
-        let now = Utc::now();
-        let (nonce, hash) = mine_block(id, now.timestamp(), &previous_hash, &data);
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
 
-        Self {
-            id,
-            hash,
-            previous_hash,
-            timestamp: now.timestamp(),
-            data,
-            nonce,
-        }
-    }
-}
+    info!("Peer Id: {}", p2p::PEER_ID.clone());
+    let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
+    let (init_sender, mut init_rcv) = mpsc::unbounded_channel();
 
-fn mine_block(id: u64, timestamp: i64, previous_hash: &str, data: &str) -> (u64, String) {
-    info!("Mining block...");
-    let mut nonce = 0;
-    loop {
-        // Rough progress indicator
-        if nonce % 100000 == 0 {
-            info!("nonce: {}", nonce);
-        }
-        // Trying to find a hash that satisfies the difficulty criteria
-        let hash = calculate_hash(id, timestamp, previous_hash, data, nonce);
-        // To check whether the calculated hash adheres difficulty criteria of starting with two zeros
-        let binary_hash = hash_to_binary_representation(&hash);
-        if binary_hash.starts_with(DIFFICULTY_PREFIX) {
-            info!(
-                "mined! nonce: {}, hash: {}, binary hash: {}",
-                nonce,
-                hex::encode(&hash),
-                binary_hash
-            );
-            return (nonce, hex::encode(hash));
-        }
-        nonce += 1;
-    }
-}
-// Create a json representation of block and put it through sha256 hash function
-fn caculate_hash(id: u64, timestamp: i64, previous_hash: &str, data: &str, nonce: u64) -> Vec<u8> {
-    let data = serde_json::json!({
-        "id": id,
-        "timestamp": timestamp,
-        "previous_hash": previous_hash,
-        "data": data,
-        "nonce": nonce // this is the number that we change to get a hash that satisfies our difficulty criteria
+    let auth_keys = Keypair::<X25519Spec>::new()
+        .into_authentic(&p2p::KEYS)
+        .expect("can create auth keys");
+
+    let transp = TokioTcpConfig::new()
+        .upgrade(upgrade::Version::V1)
+        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
+        .multiplex(mplex::MplexConfig::new())
+        .boxed();
+
+    let behaviour = p2p::AppBehaviour::new(App::new(), response_sender, init_sender.clone()).await;
+
+    let mut swarm = SwarmBuilder::new(transp, behaviour, *p2p::PEER_ID)
+        .executor(Box::new(|fut| {
+            spawn(fut);
+        }))
+        .build();
+
+    let mut stdin = BufReader::new(stdin()).lines();
+
+    Swarm::listen_on(
+        &mut swarm,
+        "/ip4/0.0.0.0/tcp/0"
+            .parse()
+            .expect("can get a local socket"),
+    )
+    .expect("swarm can be started");
+
+    spawn(async move {
+        sleep(Duration::from_secs(1)).await;
+        info!("sending init event");
+        init_sender.send(true).expect("can send init event");
     });
-    let mut hasher = Sha256::new();
-    hasher.update(data.to_string().as_bytes());
-    hasher.finalize().as_slice().to_owned()
+
+    loop {
+        let evt = {
+            select! {
+                line = stdin.next_line() => Some(p2p::EventType::Input(line.expect("can get line").expect("can read line from stdin"))),
+                response = response_rcv.recv() => {
+                    Some(p2p::EventType::LocalChainResponse(response.expect("response exists")))
+                },
+                _init = init_rcv.recv() => {
+                    Some(p2p::EventType::Init)
+                }
+                event = swarm.select_next_some() => {
+                    info!("Unhandled Swarm Event: {:?}", event);
+                    None
+                },
+            }
+        };
+
+        if let Some(event) = evt {
+            match event {
+                p2p::EventType::Init => {
+                    let peers = p2p::get_list_peers(&swarm);
+                    swarm.behaviour_mut().app.genesis();
+
+                    info!("connected nodes: {}", peers.len());
+                    if !peers.is_empty() {
+                        let req = p2p::LocalChainRequest {
+                            from_peer_id: peers
+                                .iter()
+                                .last()
+                                .expect("at least one peer")
+                                .to_string(),
+                        };
+
+                        let json = serde_json::to_string(&req).expect("can jsonify request");
+                        swarm
+                            .behaviour_mut()
+                            .floodsub
+                            .publish(p2p::CHAIN_TOPIC.clone(), json.as_bytes());
+                    }
+                }
+                p2p::EventType::LocalChainResponse(resp) => {
+                    let json = serde_json::to_string(&resp).expect("can jsonify response");
+                    swarm
+                        .behaviour_mut()
+                        .floodsub
+                        .publish(p2p::CHAIN_TOPIC.clone(), json.as_bytes());
+                }
+                p2p::EventType::Input(line) => match line.as_str() {
+                    "ls p" => p2p::handle_print_peers(&swarm),
+                    cmd if cmd.starts_with("ls c") => p2p::handle_print_chain(&swarm),
+                    cmd if cmd.starts_with("create b") => p2p::handle_create_block(cmd, &mut swarm),
+                    _ => error!("unknown command"),
+                },
+            }
+        }
+    }
 }
--- src/p2p.rs
+++ src/p2p.rs
@@ -14,13 +14,13 @@ use tokio::sync::mpsc;
 
 // p2p logic will be implemented here
 // keys and peer id intrinicis for identifying a client on the network
-pub static KEYS: Lazy = Lazy::new(identity::Keypair::generate_ed25519);
-pub static PEER_ID: Lazy = Lazy::new(|| PeerId::from(KEYS.public()));
+pub static KEYS: Lazy<identity::Keypair> = Lazy::new(identity::Keypair::generate_ed25519);
+pub static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
 // Using FloodSub protocol a simple pubsub protocol.
 // Topics are channels to subscript to, chains to send local block chain to other nodes and receive theirs. And The same for new blocks.
 
-pub static CHAIN_TOPIC: Lazy = Lazy::new(|| Topic::new("chains"));
-pub static BLOCK_TOPIC: Lazy = Lazy::new(|| Topic::new("blocks"));
+pub static CHAIN_TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("chains"));
+pub static BLOCK_TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("blocks"));
 
 // Potentially use point to point request / response model
 
@@ -30,7 +30,7 @@ pub static BLOCK_TOPIC: Lazy = Lazy::new(|| Topic::new("blocks"));
 // Struct of what to expect when given local blockchan and use to send them to other nodes
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ChainResponse {
-    pub blocks: Vec,
+    pub blocks: Vec<Block>,
     pub receiver: String,
 }
 
@@ -43,7 +43,7 @@ pub struct LocalChainRequest {
 // Event Type enum to help send events across the application and keep application state in sync with incoming and outgoing network traffic
 
 pub enum EventType {
-    LocalChainRequest(ChainResponse),
+    LocalChainResponse(ChainResponse),
     Input(String),
     Init,
 }
@@ -52,14 +52,14 @@ pub enum EventType {
 // App behaviour holds our FloodSub instacne for pub/sub cmmunication and Mdns instance, which will enalbe us to automatically find other nodes on local network
 
 // Add blockchain ap as well as channels.
-#[Derive(NetworkBehaviour)]
+#[derive(NetworkBehaviour)]
 pub struct AppBehaviour {
     pub floodsub: Floodsub,
     pub mdns: Mdns,
     #[behaviour(ignore)]
-    pub response_sender: mpsc::UnboundedSender,
+    pub response_sender: mpsc::UnboundedSender<ChainResponse>,
     #[behaviour(ignore)]
-    pub init_sender: mpsc::UnboundedSender,
+    pub init_sender: mpsc::UnboundedSender<bool>,
     #[behaviour(ignore)]
     pub app: App,
 }
@@ -67,8 +67,8 @@ pub struct AppBehaviour {
 impl AppBehaviour {
     pub async fn new(
         app: App,
-        response_sender: mpsc::UnboundedSender,
-        init_sender: mpsc::UnboundedSender,
+        response_sender: mpsc::UnboundedSender<ChainResponse>,
+        init_sender: mpsc::UnboundedSender<bool>,
     ) -> Self {
         let mut behaviour = Self {
             app,
@@ -86,16 +86,98 @@ impl AppBehaviour {
     }
 }
 
+
 // Handling incoming messages
 
+// incoming event handler
+impl NetworkBehaviourEventProcess<FloodsubEvent> for AppBehaviour {
+    fn inject_event(&mut self, event: FloodsubEvent) {
+        if let FloodsubEvent::Message(msg) = event {
+            if let Ok(resp) = serde_json::from_slice::<ChainResponse>(&msg.data) {
+                if resp.receiver == PEER_ID.to_string() {
+                    info!("Response from {}:", msg.source);
+                    resp.blocks.iter().for_each(|r| info!("{:?}", r));
+
+                    self.app.blocks = self.app.choose_chain(self.app.blocks.clone(), resp.blocks);
+                }
+            } else if let Ok(resp) = serde_json::from_slice::<LocalChainRequest>(&msg.data) {
+                info!("sending local chain to {}", msg.source.to_string());
+                let peer_id = resp.from_peer_id;
+                if PEER_ID.to_string() == peer_id {
+                    if let Err(e) = self.response_sender.send(ChainResponse {
+                        blocks: self.app.blocks.clone(),
+                        receiver: msg.source.to_string(),
+                    }) {
+                        error!("error sending response via channel, {}", e);
+                    }
+                }
+            } else if let Ok(block) = serde_json::from_slice::<Block>(&msg.data) {
+                info!("received new block from {}", msg.source.to_string());
+                self.app.try_add_block(block);
+            }
+        }
+    }
+}
+
 impl NetworkBehaviourEventProcess<MdnsEvent> for AppBehaviour {
-    fn inject_event(&mut self, event: MdnsEvent){
+    fn inject_event(&mut self, event: MdnsEvent) {
         match event {
             MdnsEvent::Discovered(discovered_list) => {
                 for (peer, _addr) in discovered_list {
-                    
+                    self.floodsub.add_node_to_partial_view(peer);
+                }
+            }
+            MdnsEvent::Expired(expired_list) => {
+                for (peer, _addr) in expired_list {
+                    if !self.mdns.has_node(&peer) {
+                        self.floodsub.remove_node_from_partial_view(&peer);
+                    }
                 }
             }
         }
+    }
+}
+
+pub fn get_list_peers(swarm: &Swarm<AppBehaviour>) -> Vec<String> {
+    info!("Discovered Peers:");
+    let nodes = swarm.behaviour().mdns.discovered_nodes();
+    let mut unique_peers = HashSet::new();
+    for peer in nodes {
+        unique_peers.insert(peer);
+    }
+    unique_peers.iter().map(|p| p.to_string()).collect()
+}
+
+pub fn handle_print_peers(swarm: &Swarm<AppBehaviour>) {
+    let peers = get_list_peers(swarm);
+    peers.iter().for_each(|p| info!("{}", p));
+}
+
+pub fn handle_print_chain(swarm: &Swarm<AppBehaviour>) {
+    info!("Local Blockchain:");
+    let pretty_json =
+        serde_json::to_string_pretty(&swarm.behaviour().app.blocks).expect("can jsonify blocks");
+    info!("{}", pretty_json);
+}
+
+pub fn handle_create_block(cmd: &str, swarm: &mut Swarm<AppBehaviour>) {
+    if let Some(data) = cmd.strip_prefix("create b") {
+        let behaviour = swarm.behaviour_mut();
+        let latest_block = behaviour
+            .app
+            .blocks
+            .last()
+            .expect("there is at least one block");
+        let block = Block::new(
+            latest_block.id + 1,
+            latest_block.hash.clone(),
+            data.to_owned(),
+        );
+        let json = serde_json::to_string(&block).expect("can jsonify request");
+        behaviour.app.blocks.push(block);
+        info!("broadcasting new block");
+        behaviour
+            .floodsub
+            .publish(BLOCK_TOPIC.clone(), json.as_bytes());
     }
 }
